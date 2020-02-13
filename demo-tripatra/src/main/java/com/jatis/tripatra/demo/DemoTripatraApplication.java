257aee480b352db9272585ab04fa89063eb1183b
Hanendyo Wicaksono <kitonus@gamil.com>
2020-02-13 11:20:28 +0700

    Prepare for release 1.0.0-RELEASE

--- demo-tripatra/src/main/java/com/jatis/tripatra/demo/DemoTripatraApplication.java
+++ demo-tripatra/src/main/java/com/jatis/tripatra/demo/DemoTripatraApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoTripatraApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting...");
+		System.out.println("Starting... Demo V1");
 		SpringApplication.run(DemoTripatraApplication.class, args);
 	}
 
